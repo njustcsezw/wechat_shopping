b42b2994351c00339e9f1b931a2193965c71fa78
njustcsezw <2990629037@qq.com>
2019-06-24 20:26:34 +0800

    commit BuyerProductController

--- src/main/java/com/njustz/wechat_shopping/controller/BuyerOrderController.java
+++ src/main/java/com/njustz/wechat_shopping/controller/BuyerOrderController.java
@@ -1,8 +1,26 @@
 package com.njustz.wechat_shopping.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author njustz
  * @date 2019/6/24
  */
+
+@RestController
+@RequestMapping("/buyer/order")
+@Slf4j
 public class BuyerOrderController {
+
+    //创建订单
+
+
+    //订单列表
+
+
+    //取消订单
+
+
 }
---
+++ src/main/java/com/njustz/wechat_shopping/form/OrderForm.java
@@ -0,0 +1,8 @@
+package com.njustz.wechat_shopping.form;
+
+/**
+ * @author njustz
+ * @date 2019/6/24
+ */
+public class OrderForm {
+}
